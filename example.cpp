074d4044bfb01aa095d854cd36eb00db22d2f950
Local-Micro <Local-Micro@outlook.com>
2024-07-16 08:12:27 +0800

    添加example.cpp来展示效果

--- example.cpp
+++ example.cpp
@@ -1,3 +1,4 @@
+// fh
 /**
 **********************************************************************************************************************************************************************************************************************************
 * @file:	example.cpp
@@ -5,7 +6,117 @@
 * @date:	2024年07月16日 07:45:29 星期二
 * @brief:	
 **********************************************************************************************************************************************************************************************************************************
-**/ // fh
-#include // inc
-using namespace // usi
-mai
+**/
+
+// #ifn
+#ifndef __EXAMPLE__H__
+#define __EXAMPLE__H__
+
+#endif  // !__EXAMPLE__H__
+
+// #def
+#define    
+
+// inc
+#include
+
+// usi
+using namespace
+
+// mai
+int	main(int argc, char const *argv[]) {
+    for
+
+    return 0;
+}
+
+// for
+for (int  = 0;  < length; ++) {
+    
+}
+
+// fore
+for (auto & : ) {
+    
+}
+
+// do
+do {
+    
+} while();
+
+// whi
+while () {
+    
+}
+
+// if
+if () {
+    
+}
+
+// els
+else {
+    
+}
+
+// elif
+else if () {
+    
+}
+
+// enu
+enum  {
+    
+};
+
+// cla
+class  {
+
+};
+
+// classi
+class  :public  {
+
+};
+
+// anm
+namespace  {
+    
+}//
+
+// stru
+struct  {
+    
+};
+
+// swi
+switch () {
+    case :
+        
+        break;
+
+    default:
+        
+        break;
+}
+
+// cas
+case :
+    
+    break;
+
+// try
+try {
+    
+} catch () {
+    
+}
+
+// uni
+union  {
+    
+};
+
+// tem
+template <typename >
